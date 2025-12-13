cc14e1325359e566910ade4b3e12e9297385586d
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-13 19:53:25 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Stopwatch from './Stopwatch/Stopwatch'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full'>
+      <Stopwatch/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Stopwatch/Stopwatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stopwatch = () => {
+  return (
+    <div className='h-screen w-full bg-black text-white flex items-center justify-center'>
+        <h1>hey hunain how ar u</h1>
+    </div>
+  )
+}
+
+export default Stopwatch
