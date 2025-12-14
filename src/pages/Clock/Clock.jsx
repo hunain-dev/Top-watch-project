f920dd3c2d632a1552a7620bd2a01621e7f44229
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-14 16:08:39 +0500

    updated

--- src/pages/Clock/Clock.jsx
+++ src/pages/Clock/Clock.jsx
@@ -1,26 +1,38 @@
+import { useEffect, useState } from "react";
 import Bottombar from "../../Components/Bottombar";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { IoMdAddCircle } from "react-icons/io";
-// import axios from "axios";
 const Clock = () => {
-  // const [showworldtime, setshowworldtime] = useState([])
+  const [date, setDate] = useState(new Date());
 
-  // useEffect(() => {
-  //   const apihandler = async () => {
-  //     try {
-  //       const response = await axios.get("https://worldtimeapi.org/api/timezone");
-  //       const data = response.data
-  //       console.log(data);
-  //       setshowworldtime(data)
-  //     } catch (error) {
-  //       console.log("API is not running brother", error);
-  //     }
-  //   };
-  
-  //   apihandler();
-  // }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new date())
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [])
   
 
+  const formatTime  = (date) =>{
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    const minStr = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hours}:${minStr} ${ampm}`;
+  }
+
+  const formatDate = (date) => {
+    const months = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+  };
 
 
   
@@ -29,20 +41,21 @@ const Clock = () => {
  
  <div className="max-h-[85vh] min-w-[21%] rounded-2xl bg-linear-to-b from-yellow-50 to-white overflow-hidden">
         <div className="min-h-[76vh] flex items-start flex-col justify-start w-full ">
-          <div className="py-2 px-2 relative  w-full  flex items-end justify-end">
+          {/* <div className="py-2 px-2 relative  w-full  flex items-end justify-end">
           <IoMdAddCircle className="text-2xl cursor-pointer" />
 
         
 
 
-          </div>
+          </div> */}
           <div className="flex-col mt-15 Actay t flex px-2  items-center justify-center w-full">
             <div className="flex mt-3 items-center justify-center gap-2">
             <CiLocationArrow1 />
             <h3>Pakistan.</h3>
             </div>
-            <h5 className="Actay mt-2 text-[2vh] opacity-60">December 14 January 2025</h5>
-            <h5 className="Actay  font-semibold text-4xl mt-5">02:29 PM</h5>
+
+            <h5 className="Actay mt-2 text-[2vh] opacity-60">{formatDate(date)}</h5>
+            <h5 className="Actay  font-semibold text-4xl mt-5">{formatTime(date)}</h5>
           
           </div>
        
